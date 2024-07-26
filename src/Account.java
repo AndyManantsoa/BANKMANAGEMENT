6258ee072a7d34778cf92b3278a9b86149a5325a
AndyManantsoa <manantsoaadrs@gmail.com>
2024-07-26 13:53:05 +0530

    new

---
+++ src/Account.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+
+public class Account{
+    Scanner scan = new Scanner(System.in);
+    int id, phone, balance;
+    static int ifsc = 2024;
+    String name, email, type, accountNumber;
+    static String branchName ="BARODA";
+    static int count = 0;
+
+    public void addAccount()
+    {
+        System.out.println("Enter your name :");
+        name = scan.next();
+        System.out.println("Enter your email :");
+        email = scan.next();
+        System.out.println("Enter your phone number :");
+        phone = scan.nextInt();
+        System.out.println("Enter your account type :");
+
+        System.out.println("""
+                            \t1 - current
+                            \t2 - saving
+                            """);
+
+        int choice=scan.nextInt();
+
+        while(!(choice ==1||choice ==2)){
+            System.out.println("Invalid choice! Enter 1 or 2");
+            choice = scan.nextInt();
+        }
+
+        if(choice==1){
+            type = "Current";
+        }else {
+            type = "Saving";
+        }
+
+        id = ++count;
+        balance = 0;
+        accountNumber = "0MCB"+(int)(24000000+id);
+
+        displayDetails();
+
+        System.out.println("Account created successfully :) ");
+    }
+
+
+    public void displayDetails()
+    {
+        System.out.println("Details for account number = "+this.accountNumber+"\n");
+        System.out.println("Name : "+this.name);
+        System.out.println("Email : "+this.email);
+        System.out.println("Phone : "+this.phone);
+        System.out.println("Type : "+this.type);
+        System.out.println("Branch : "+branchName);
+        System.out.println("IFSC : "+ifsc);
+    }
+
+    public void update(){
+        System.out.println("Enter new name :");
+        name = scan.next();
+        System.out.println("Enter new email :");
+        email = scan.next();
+        System.out.println("Enter new phone number :");
+        phone = scan.nextInt();
+
+        System.out.println("Account updated successfully :) ");
+        displayDetails();
+        System.out.println("***************************************************");
+
+    }
+
+    public void displayBalance(){
+        System.out.println("Balance in account number "+this.accountNumber+" = "+this.balance);
+    }
+
+    public void credit(){
+        System.out.println("Enter the amount you want to credit:");
+        int amount=scan.nextInt();
+        this.balance += amount;
+        System.out.println("Account number : "+this.accountNumber+" credited successfully with amount "+amount);
+        displayBalance();
+    }
+
+    public void debit(){
+        System.out.println("Enter the amount you want to debit:");
+        int amount=scan.nextInt();
+        if(amount<this.balance){
+            this.balance -= amount;
+            System.out.println("Account number : "+this.accountNumber+" debited successfully with amount "+amount);
+            displayBalance();
+        }else{
+            System.out.println("Insufficient fund in your account");
+        }
+    }
+
+
+}
---
+++ src/Management.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+
+public class Management{
+    Scanner scan = new Scanner(System.in);
+    Account [] accounts = new Account[100];
+    Account temp = new Account();
+    int i=0;
+
+
+    public Account validation(String number){
+        for(int a=0; a<i; a++)
+        {
+            if(accounts[a].accountNumber.equals(number))
+            {
+                return accounts[a];
+            }
+        }
+        return null;
+    }
+
+    public void add()
+    {
+        temp.addAccount();
+        accounts[i] = temp;
+        i++;
+
+    }
+
+    public void display(String number)
+    {
+        Account temp = validation(number);
+        if(temp!=null){
+            temp.displayDetails();
+        }else{
+            System.out.println("Account not found\n");
+        }
+
+    }
+
+    public void display(){
+        if(i<=0){
+            System.out.println("There is no account registered");
+        }
+        for(int x =0 ; x<i;x++){
+            System.out.println("Account "+(x+1)+": ");
+            accounts[x].displayDetails();
+            System.out.println();
+        }
+    }
+
+    public void update(String number){
+        Account temp = validation(number);
+        if(temp!=null){
+            temp.update();
+        }else{
+            System.out.println("Account not found\n");
+        }
+    }
+
+    public void transaction(String number){
+        Account temp = validation(number);
+        if(temp!=null){
+            temp.displayDetails();
+            System.out.println(
+                    """
+                    Enter the type of transaction:\
+                    \t1- Credit
+                    \t2- Debit
+                    \t3- View balance
+                    """);
+            int trans= scan.nextInt();
+
+            while(!(trans ==1|| trans ==2|| trans ==3)){
+                System.out.println("Invalid choice! Enter 1 or 2 or 3");
+                trans = scan.nextInt();
+            }
+
+            if(trans == 1){
+                temp.credit();
+            }else if(trans ==2){
+                temp.debit();
+            }else{
+                temp.displayBalance();
+            }
+        }else{
+            System.out.println("Account not found\n");
+        }
+
+    }
+}
